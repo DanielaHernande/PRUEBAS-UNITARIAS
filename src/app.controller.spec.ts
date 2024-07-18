6218d3af37cc22122cb53b98d244330522c7a084
DanielaHernande <danielajimenezhernandez6@gmail.com>
2024-07-18 08:47:50 -0500

    Feat: Test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -19,4 +19,29 @@ describe('AppController', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
+
+  describe('Test Fortuna', () => {
+
+    it('Validete Type of return equal number', () => {
+      expect(typeof appController.getFortune()).toBe('number');
+    });
+
+    // Que tenga 4 digitos
+    it('Validete size of fortune number == 4 digits', () => {
+      expect(typeof appController.getFortune().toString().length).toBe(4);
+    });
+
+    // Validar que sea igua a 4 digitos
+    it('Validete size of fortune number <= 4 digits', () => {
+      expect(typeof appController.getFortune().toString().length).toBeLessThanOrEqual(4);
+    });
+
+    // Validar que sel numero sea aleatorio
+    it('Validete size of fortune number <= 4 digits', () => {
+      const v1: number = appController.getFortune();
+      const v2: number = appController.getFortune();
+      expect(v1).not.toEqual(v2);
+    });
+  });
+
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,11 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/fortune')
+  getFortune(): number {
+    return this.appService.getFortune();
+  }
+
+
 }
